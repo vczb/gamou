a901dba43824e71411ae6a6389f5356250ada909
Vinicius CZB <viniczb@gmail.com>
2023-08-07 19:52:04 -0300

    Create global mock

---
+++ src/__mock__/data.ts
@@ -0,0 +1,253 @@
+export default {
+  avatar: {
+    alt: "A random image",
+    src: "https://picsum.photos/200/300",
+  },
+  company: {
+    uid: "a1b2b3d4e5f6g7h8i9",
+    name: "Urban Palate Restaurant",
+    workhour: [
+      {
+        open_time: "11:00",
+        close_time: "15:00",
+        week_day_id: 1,
+      },
+      {
+        open_time: "11:00",
+        close_time: "15:00",
+        week_day_id: 2,
+      },
+      {
+        open_time: "11:00",
+        close_time: "15:00",
+        week_day_id: 3,
+      },
+      {
+        open_time: "11:00",
+        close_time: "15:00",
+        week_day_id: 4,
+      },
+      {
+        open_time: "11:00",
+        close_time: "15:00",
+        week_day_id: 5,
+      },
+      {
+        open_time: "11:00",
+        close_time: "15:00",
+        week_day_id: 6,
+      },
+      {
+        open_time: "11:00",
+        close_time: "15:00",
+        week_day_id: 7,
+      },
+      {
+        open_time: "18:00",
+        close_time: "22:00",
+        week_day_id: 1,
+      },
+      {
+        open_time: "18:00",
+        close_time: "22:00",
+        week_day_id: 2,
+      },
+      {
+        open_time: "18:00",
+        close_time: "22:00",
+        week_day_id: 3,
+      },
+      {
+        open_time: "18:00",
+        close_time: "22:00",
+        week_day_id: 4,
+      },
+      {
+        open_time: "18:00",
+        close_time: "22:00",
+        week_day_id: 5,
+      },
+      {
+        open_time: "18:00",
+        close_time: "22:00",
+        week_day_id: 6,
+      },
+      {
+        open_time: "18:00",
+        close_time: "22:00",
+        week_day_id: 7,
+      },
+    ],
+  },
+  categories: [
+    {
+      uid: "italian",
+      name: "Italian",
+      image: "https://picsum.photos/200/300",
+    },
+    {
+      uid: "steakhouse",
+      name: "Steakhouse",
+      image: "https://picsum.photos/200/300",
+    },
+    {
+      uid: "sushi",
+      name: "Japanese",
+      image: "https://picsum.photos/200/300",
+    },
+    {
+      uid: "cafe",
+      name: "Coffee",
+      image: "https://picsum.photos/200/300",
+    },
+    {
+      uid: "seafood",
+      name: "Seafood",
+      image: "https://picsum.photos/200/300",
+    },
+  ],
+  category_product_list: [
+    {
+      uid: "italian",
+      name: "Italian Cuisine",
+      products: [
+        {
+          uid: "123",
+          title: "Spaghetti Carbonara",
+          image: "https://picsum.photos/200/300",
+          description:
+            "A classic Italian pasta dish featuring creamy eggs, crispy bacon, grated Pecorino cheese, and freshly cracked black pepper.",
+          price: 14.99,
+        },
+        {
+          uid: "456",
+          title: "Margherita Pizza",
+          image: "https://picsum.photos/200/300",
+          description:
+            "A traditional pizza topped with fresh tomato sauce, Mozzarella cheese, tomatoes, and basil leaves.",
+          price: 12.99,
+        },
+        {
+          uid: "789",
+          title: "Chicken Alfredo",
+          image: "https://picsum.photos/200/300",
+          description:
+            "Tender grilled chicken served with creamy Alfredo sauce and fettuccine pasta.",
+          price: 17.99,
+        },
+      ],
+    },
+    {
+      uid: "steakhouse",
+      name: "Steakhouse",
+      products: [
+        {
+          uid: "123",
+          title: "Ribeye Steak",
+          image: "https://picsum.photos/200/300",
+          description:
+            "A juicy and flavorful ribeye steak cooked to perfection, served with garlic mashed potatoes and sautéed vegetables.",
+          price: 29.99,
+        },
+        {
+          uid: "456",
+          title: "Filet Mignon",
+          image: "https://picsum.photos/200/300",
+          description:
+            "Tender and succulent filet mignon grilled to your preferred level of doneness, accompanied by a red wine reduction sauce.",
+          price: 34.99,
+        },
+        {
+          uid: "789",
+          title: "New York Strip",
+          image: "https://picsum.photos/200/300",
+          description:
+            "A classic New York strip steak cooked to order and served with a loaded baked potato and grilled asparagus.",
+          price: 27.99,
+        },
+      ],
+    },
+    {
+      uid: "sushi",
+      name: "Sushi and Japanese",
+      products: [
+        {
+          uid: "123",
+          title: "California Roll",
+          image: "https://picsum.photos/200/300",
+          description:
+            "A popular sushi roll containing imitation crab, avocado, cucumber, and tobiko (fish roe).",
+          price: 9.99,
+        },
+        {
+          uid: "456",
+          title: "Sashimi Platter",
+          image: "https://picsum.photos/200/300",
+          description:
+            "An assortment of fresh sashimi slices, including tuna, salmon, and yellowtail.",
+          price: 18.99,
+        },
+        {
+          uid: "789",
+          title: "Tempura Udon",
+          image: "https://picsum.photos/200/300",
+          description:
+            "A comforting bowl of udon noodles served in a savory broth with tempura shrimp and vegetables.",
+          price: 14.99,
+        },
+      ],
+    },
+    {
+      uid: "cafe",
+      name: "Café and Coffee Shops",
+      products: [
+        {
+          uid: "123",
+          title: "Cappuccino",
+          image: "https://picsum.photos/200/300",
+          description:
+            "A classic Italian coffee beverage made with equal parts espresso, steamed milk, and milk foam.",
+          price: 4.49,
+        },
+        {
+          uid: "456",
+          title: "Croissant",
+          image: "https://picsum.photos/200/300",
+          description:
+            "A flaky and buttery pastry that pairs perfectly with a cup of coffee or tea.",
+          price: 2.99,
+        },
+        {
+          uid: "789",
+          title: "Avocado Toast",
+          image: "https://picsum.photos/200/300",
+          description:
+            "Sliced avocado on toasted multigrain bread, topped with red pepper flakes and a drizzle of olive oil.",
+          price: 7.99,
+        },
+      ],
+    },
+    {
+      uid: "seafood",
+      name: "Seafood Delights",
+      products: [
+        {
+          uid: "123",
+          title: "Grilled Salmon",
+          image: "https://picsum.photos/200/300",
+          description:
+            "A succulent salmon fillet seasoned and grilled to perfection, served with lemon-dill sauce.",
+          price: 19.99,
+        },
+        {
+          uid: "456",
+          title: "Lobster Bisque",
+          image: "https://picsum.photos/200/300",
+          description:
+            "A rich and creamy lobster soup with chunks of tender lobster meat, perfect for seafood enthusiasts.",
+          price: 10.99,
+        },
+      ],
+    },
+  ],
+} as const;
