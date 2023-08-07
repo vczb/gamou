626f24248c5ad6557574e2feb3e9463a68d645a9
Vinicius CZB <viniczb@gmail.com>
2023-08-07 19:51:22 -0300

    Fix map key on ProductList component

--- src/components/ProductList/index.tsx
+++ src/components/ProductList/index.tsx
@@ -8,8 +8,11 @@ const ProductList = ({ products }: ProductListProps) => {
   return (
     <div className="grid gap-4">
       {products.map((product) => (
-        <div className="mb-4 pb-4 border-b-2 border-dashed border-blueGray-200 last:border-none last:mb-0 last:pb-0">
-          <Product {...product} key={product.uid} />
+        <div
+          className="mb-4 pb-4 border-b-2 border-dashed border-blueGray-200 last:border-none last:mb-0 last:pb-0"
+          key={product.uid}
+        >
+          <Product {...product} />
         </div>
       ))}
     </div>
