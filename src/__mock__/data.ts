96bff02b1b909a90c217c03f499845224b3b6d7e
Vini <viniczb@gmail.com>
2024-07-21 11:15:44 +0200

    Improve mock data

--- src/__mock__/data.ts
+++ src/__mock__/data.ts
@@ -1,12 +1,8 @@
 export default {
-  avatar: {
-    alt: "A random image",
-    src: "https://picsum.photos/200/300",
-  },
   company: {
     uid: "a1b2b3d4e5f6g7h8i9",
-    image: "https://picsum.photos/200/300",
-    name: "Urban Palate Restaurant",
+    image: "http://localhost:3000/uploads/company_image.webp",
+    name: "Super Gostoso Restaurante",
     currency: "R$",
     workhour: [
       {
@@ -83,171 +79,101 @@ export default {
   },
   categories: [
     {
-      uid: "italian",
-      name: "Italian",
-      image: "https://picsum.photos/200/300",
+      uid: "vegetarian",
+      name: "Vegetariana",
+      image: "http://localhost:3000/uploads/veg_category.webp",
     },
     {
-      uid: "steakhouse",
-      name: "Steakhouse",
-      image: "https://picsum.photos/200/300",
+      uid: "soup",
+      name: "Sopas e Cremes",
+      image: "http://localhost:3000/uploads/creme_abobora.webp",
     },
     {
-      uid: "sushi",
-      name: "Japanese",
-      image: "https://picsum.photos/200/300",
-    },
-    {
-      uid: "cafe",
-      name: "Coffee",
-      image: "https://picsum.photos/200/300",
-    },
-    {
-      uid: "seafood",
-      name: "Seafood",
-      image: "https://picsum.photos/200/300",
+      uid: "drinks",
+      name: "Bebidas",
+      image: "http://localhost:3000/uploads/coca_cola.webp",
     },
   ],
   category_product_list: [
     {
-      uid: "italian",
-      name: "Italian Cuisine",
+      uid: "vegetarian",
+      name: "Comida Vegetariana",
       products: [
         {
           uid: "123",
-          title: "Spaghetti Carbonara",
-          image: "https://picsum.photos/200/300",
+          title: "Almodegas à grega",
+          image: "http://localhost:3000/uploads/almodega_grega.webp",
           description:
-            "A classic Italian pasta dish featuring creamy eggs, crispy bacon, grated Pecorino cheese, and freshly cracked black pepper.",
-          price: 14.99,
+            "Almôndegas 100% plant-based com arroz à grega, coalhada vegetal de amêndoas e tomate grape. Peso 271g",
+          price: 35.06,
         },
         {
           uid: "456",
-          title: "Margherita Pizza",
-          image: "https://picsum.photos/200/300",
+          title: "Burger Parmegiana",
+          image: "http://localhost:3000/uploads/burger_parmegiana.webp",
           description:
-            "A traditional pizza topped with fresh tomato sauce, Mozzarella cheese, tomatoes, and basil leaves.",
-          price: 12.99,
+            "Burger feito de plantas, coberto por molho de tomate, queijo de castanha, tomate italiano e acompanhado de batatas assadas com alecrim. Peso: 318gr",
+          price: 31.26,
         },
         {
           uid: "789",
-          title: "Chicken Alfredo",
-          image: "https://picsum.photos/200/300",
+          title: "Feijoada Vegana",
+          image: "http://localhost:3000/uploads/feijoada_veg.webp",
           description:
-            "Tender grilled chicken served with creamy Alfredo sauce and fettuccine pasta.",
-          price: 17.99,
+            "Feijoada com arroz cateto, couve e farofa de mandioca. Peso: 315gr",
+          price: 17.94,
         },
       ],
     },
     {
-      uid: "steakhouse",
-      name: "Steakhouse",
+      uid: "soup",
+      name: "Sopas",
       products: [
         {
-          uid: "123",
-          title: "Ribeye Steak",
-          image: "https://picsum.photos/200/300",
+          uid: "234",
+          title: "Cremes de Ervilha",
+          image: "http://localhost:3000/uploads/creme_ervilha.webp",
           description:
-            "A juicy and flavorful ribeye steak cooked to perfection, served with garlic mashed potatoes and sautéed vegetables.",
-          price: 29.99,
+            "Sopa de ervilha com legumes e alho. Peso 271g",
+          price: 19.00,
         },
         {
-          uid: "456",
-          title: "Filet Mignon",
-          image: "https://picsum.photos/200/300",
+          uid: "567",
+          title: "Cremes de Abóbora",
+          image: "http://localhost:3000/uploads/creme_abobora.webp",
           description:
-            "Tender and succulent filet mignon grilled to your preferred level of doneness, accompanied by a red wine reduction sauce.",
-          price: 34.99,
+            "Sopa de abóbora com legumes e alho. Peso 271g",
+          price: 19.00,
         },
         {
-          uid: "789",
-          title: "New York Strip",
-          image: "https://picsum.photos/200/300",
+          uid: "890",
+          title: "Sopas de Lentilha",
+          image: "http://localhost:3000/uploads/creme_lentilha.webp",
           description:
-            "A classic New York strip steak cooked to order and served with a loaded baked potato and grilled asparagus.",
-          price: 27.99,
+            "Sopa de lentilha com legumes e alho. Peso 271g",
+          price: 19.00,
         },
       ],
     },
     {
-      uid: "sushi",
-      name: "Sushi and Japanese",
+      uid: "drinks",
+      name: "Bebidas",
       products: [
         {
-          uid: "123",
-          title: "California Roll",
-          image: "https://picsum.photos/200/300",
+          uid: "a123",
+          title: "Coca Cola",
+          image: "http://localhost:3000/uploads/coca_cola.webp",
           description:
-            "A popular sushi roll containing imitation crab, avocado, cucumber, and tobiko (fish roe).",
-          price: 9.99,
+            "Coca Cola lata 300ml",
+          price: 3.00,
         },
         {
-          uid: "456",
-          title: "Sashimi Platter",
-          image: "https://picsum.photos/200/300",
+          uid: "a321",
+          title: "Agua",
+          image: "http://localhost:3000/uploads/agua_mineral.webp",
           description:
-            "An assortment of fresh sashimi slices, including tuna, salmon, and yellowtail.",
-          price: 18.99,
-        },
-        {
-          uid: "789",
-          title: "Tempura Udon",
-          image: "https://picsum.photos/200/300",
-          description:
-            "A comforting bowl of udon noodles served in a savory broth with tempura shrimp and vegetables.",
-          price: 14.99,
-        },
-      ],
-    },
-    {
-      uid: "cafe",
-      name: "Café and Coffee Shops",
-      products: [
-        {
-          uid: "123",
-          title: "Cappuccino",
-          image: "https://picsum.photos/200/300",
-          description:
-            "A classic Italian coffee beverage made with equal parts espresso, steamed milk, and milk foam.",
-          price: 4.49,
-        },
-        {
-          uid: "456",
-          title: "Croissant",
-          image: "https://picsum.photos/200/300",
-          description:
-            "A flaky and buttery pastry that pairs perfectly with a cup of coffee or tea.",
-          price: 2.99,
-        },
-        {
-          uid: "789",
-          title: "Avocado Toast",
-          image: "https://picsum.photos/200/300",
-          description:
-            "Sliced avocado on toasted multigrain bread, topped with red pepper flakes and a drizzle of olive oil.",
-          price: 7.99,
-        },
-      ],
-    },
-    {
-      uid: "seafood",
-      name: "Seafood Delights",
-      products: [
-        {
-          uid: "123",
-          title: "Grilled Salmon",
-          image: "https://picsum.photos/200/300",
-          description:
-            "A succulent salmon fillet seasoned and grilled to perfection, served with lemon-dill sauce.",
-          price: 19.99,
-        },
-        {
-          uid: "456",
-          title: "Lobster Bisque",
-          image: "https://picsum.photos/200/300",
-          description:
-            "A rich and creamy lobster soup with chunks of tender lobster meat, perfect for seafood enthusiasts.",
-          price: 10.99,
+            "Agua 500ml",
+          price: 2.50,
         },
       ],
     },
