b67165ed0bef5c4708c75d4a84b1e2d176cc8bed
Vini <viniczb@gmail.com>
2024-07-29 11:41:49 +0200

    Fix storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
 
 const config: StorybookConfig = {
   stories: ["../src/**/stories.mdx", "../src/**/stories.@(js|jsx|mjs|ts|tsx)"],
@@ -15,5 +16,13 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "../src"),
+    };
+    return config;
+  },
 };
+
 export default config;
