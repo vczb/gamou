59e8b6cf05fd0746deb21960cfb75a7ce96bef83
Vini <viniciuszucatti@gmail.com>
2024-08-26 23:58:13 +0200

    Add icon switcher in SearchField

--- src/components/SearchField/index.tsx
+++ src/components/SearchField/index.tsx
@@ -1,19 +1,26 @@
-import { FormEvent, InputHTMLAttributes, useCallback } from "react";
+import { FormEvent, InputHTMLAttributes, useCallback, useState } from "react";
 import TextField from "../TextField";
 import Search from "../../icons/Search";
 import Button from "../Button";
+import Trash from "@/icons/Trash";
 
 export type SearchFieldProps = {
   handleSearch: (searchTerm: string) => void;
 } & InputHTMLAttributes<HTMLInputElement>;
 
 const SearchField = ({ handleSearch, ...props }: SearchFieldProps) => {
+  const [searchIcon, setSearchIcon] = useState(true);
+
   const onSubmit = useCallback(
     (e: FormEvent<HTMLFormElement>) => {
       e.preventDefault();
       const data = new FormData(e.currentTarget);
 
       const searchTerm = data.get("search-term") || "";
+
+      setSearchIcon(!searchTerm);
+
+      e.currentTarget.reset();
 
       return handleSearch(searchTerm as string);
     },
@@ -29,7 +36,11 @@ const SearchField = ({ handleSearch, ...props }: SearchFieldProps) => {
         name="search-term"
       />
       <Button className="rounded-none rounded-e-lg" type="submit">
-        <Search className="w-6 h-6" />
+        {searchIcon ? (
+          <Search className="w-6 h-6" />
+        ) : (
+          <Trash className="w-6 h-6" />
+        )}
       </Button>
     </form>
   );
