750308d97bf913697d6dace509b54b8dfed2907c
Vini <viniciuszucatti@gmail.com>
2024-09-09 23:37:30 +0200

    Create FlashMessage component

---
+++ src/components/FlashMessage/index.tsx
@@ -0,0 +1,39 @@
+import { useId, useMemo } from "react";
+
+type FlashMessage = {
+  type: "alert" | "success";
+  msg: string;
+};
+
+const FlashMessage = ({ type, msg }: FlashMessage) => {
+  const bgColor = useMemo(
+    () => (type === "alert" ? "bg-red-500" : "bg-emerald-400"),
+    [type]
+  );
+
+  const id = useId();
+
+  return (
+    <div>
+      <input type="checkbox" className="flash-banner hidden peer" id={id} />
+      <label
+        className={`flash-banner w-full fixed top-0 z-50 cursor-pointer flex items-center justify-between p-2 ${bgColor} shadow text-white peer-checked:hidden`}
+        title="Close"
+        htmlFor={id}
+      >
+        {msg}
+        <svg
+          className="fill-current text-white"
+          xmlns="http://www.w3.org/2000/svg"
+          width="18"
+          height="18"
+          viewBox="0 0 18 18"
+        >
+          <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+        </svg>
+      </label>
+    </div>
+  );
+};
+
+export default FlashMessage;
