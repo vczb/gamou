80e8ce97ed6a46dd90a2a18d8e089193a2f4a699
Vini <viniciuszucatti@gmail.com>
2024-09-28 18:09:49 -0300

    Add error handler to upload route

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -4,29 +4,37 @@ import fs from 'fs';
 import path from 'path';
 
 export async function POST(request: Request) {
-  const userId = request.headers.get("user-id");
+  try {
 
-  if (!userId) {
-    return NextResponse.json({ error: 'User not authenticated' }, { status: 401 });
-  }
+    const userId = request.headers.get("user-id");
+
+    if (!userId) {
+      return NextResponse.json({ error: 'Não foi possível identificar o usuário' }, { status: 401 });
+    }
+    
+    const formData = await request.formData();
+    const file = formData.get('file') as File;
   
-  const formData = await request.formData();
-  const file = formData.get('file') as File;
-
-  if (!file) {
-    return NextResponse.json({ error: 'No file uploaded' }, { status: 400 });
+    if (!file) {
+      return NextResponse.json({ error: 'Falha ao salvar o arquivo' }, { status: 400 });
+    }
+  
+    const userDir = path.join(process.cwd(), 'public/uploads', userId);
+    fs.mkdirSync(userDir, { recursive: true });
+  
+    const filePath = path.join(userDir, `${file.name.split('.')[0]}.png`);
+    const arrayBuffer = await file.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+  
+    await sharp(buffer)
+      .png({ quality: 80 })
+      .toFile(filePath);
+  
+    return NextResponse.json({ status: 200, success: true, filePath, message: 'Arquivo salvo com sucesso!' });
+    
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ message: "Erro interno." }, { status: 500 });
   }
 
-  const userDir = path.join(process.cwd(), 'public/uploads', userId);
-  fs.mkdirSync(userDir, { recursive: true });
-
-  const filePath = path.join(userDir, `${file.name.split('.')[0]}.png`);
-  const arrayBuffer = await file.arrayBuffer();
-  const buffer = Buffer.from(arrayBuffer);
-
-  await sharp(buffer)
-    .png({ quality: 80 })
-    .toFile(filePath);
-
-  return NextResponse.json({ success: true, filePath, message: 'File uploaded and converted to PNG successfully' });
 }
