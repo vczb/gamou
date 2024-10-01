0cd70d40d907e822edf0e7743a91ea73d6e79a71
Vini <viniciuszucatti@gmail.com>
2024-10-01 13:57:57 -0300

    Migrate: update products to reference companies

---
+++ src/database/migrations/20241001165709_alter_products_dependency_to_companies.js
@@ -0,0 +1,31 @@
+exports.up = function (knex) {
+  return knex.schema.alterTable("products", function (table) {
+    // Remove the user_id column
+    table.dropColumn("user_id");
+
+    // Add the company_id column and reference the companies table
+    table
+      .integer("company_id")
+      .unsigned()
+      .references("id")
+      .inTable("companies")
+      .onDelete("CASCADE")
+      .notNullable();
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.alterTable("products", function (table) {
+    // Drop the company_id column
+    table.dropColumn("company_id");
+
+    // Re-add the user_id column and reference the users table
+    table
+      .integer("user_id")
+      .unsigned()
+      .references("id")
+      .inTable("users")
+      .onDelete("CASCADE")
+      .notNullable();
+  });
+};
