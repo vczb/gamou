448ff9f5315fe431c3a66aea353316b975ff0905
Vini <viniciuszucatti@gmail.com>
2024-10-08 21:40:05 -0300

    Move upload method to an controller

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,41 +1,22 @@
-import sharp from 'sharp';
 import { NextResponse } from 'next/server';
-import fs from 'fs';
-import path from 'path';
+import { UploaderController } from '@/controllers/UploadController';
 
 export async function POST(request: Request) {
   try {
-    // Extract user ID from headers
-    const userId = request.headers.get("user-id");
 
-    if (!userId) {
-      return NextResponse.json({ error: 'Não foi possível identificar o usuário' }, { status: 401 });
-    }
 
-    // Parse form data to get the file
     const formData = await request.formData();
     const file = formData.get('file') as File;
 
-    if (!file) {
-      return NextResponse.json({ error: 'Falha ao salvar o arquivo' }, { status: 400 });
-    }
+    const controller = new UploaderController()
 
-    // Create user directory if it doesn't exist
-    const userDir = path.join(process.cwd(), 'public/uploads', userId);
-    fs.mkdirSync(userDir, { recursive: true });
+    const response = await controller.createFileByToken({file})
 
-    // Define file path and convert file to buffer
-    const filePath = path.join(userDir, `${file.name.split('.')[0]}.png`);
-    const arrayBuffer = await file.arrayBuffer();
-    const buffer = Buffer.from(arrayBuffer);
+    const data = response.data
+    const message = response.message
+    const filePath = data?.filePath || ""
 
-    // Process and save the image using sharp
-    await sharp(buffer)
-      .png({ quality: 80 })
-      .toFile(filePath);
-
-    // Return success response
-    return NextResponse.json({ status: 200, success: true, filePath, message: 'Arquivo salvo com sucesso!' });
+    return NextResponse.json({ status: 200, success: true, filePath, message });
 
   } catch (error) {
     console.error(error);
---
+++ src/controllers/UploadController.ts
@@ -0,0 +1,46 @@
+import sharp from 'sharp';
+import { NextResponse } from 'next/server';
+import fs from 'fs';
+import path from 'path';
+import { BaseController } from './BaseController';
+
+export class UploaderController extends BaseController {
+
+  async createFileByToken({
+    file
+  }: {file: File}) {
+    try {
+
+      if (!file) {
+        return this.badRequest("Erro ao processar arquivo")
+      }
+      
+      const userId = await this.verifyToken()
+
+      if (!userId) {
+        return this.unprocessableEntity("User not found.");
+      }
+
+      const userDir = path.join(process.cwd(), 'public/uploads', String(userId));
+      fs.mkdirSync(userDir, { recursive: true });
+
+      const filePath = path.join(userDir, `${file.name.split('.')[0]}.png`);
+      const arrayBuffer = await file.arrayBuffer();
+      const buffer = Buffer.from(arrayBuffer);
+
+      await sharp(buffer)
+        .png({ quality: 80 })
+        .toFile(filePath);
+
+      const data = {
+        filePath
+      }
+
+      return this.ok("Arquivo criado com sucesso!", data)
+
+    } catch (error) {
+      console.error(error);
+      return this.serverError("Erro ao criar o arquivo")
+    }
+  }
+}
