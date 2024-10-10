12ceddf1fb872f40f21ad1132a0bf68e634174d4
Vini <viniciuszucatti@gmail.com>
2024-10-10 20:01:57 -0300

    Add helper text to TextField

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -1,13 +1,43 @@
-import { InputHTMLAttributes } from "react";
+import { InputHTMLAttributes, useMemo } from "react";
 
-export type TextFieldProps = InputHTMLAttributes<HTMLInputElement>;
+export type TextFieldProps = {
+  helperText?: string;
+  helperVariant?: "alert" | "success";
+} & InputHTMLAttributes<HTMLInputElement>;
 
-const TextField = ({ className = "", ...props }: TextFieldProps) => {
+const TextField = ({
+  className = "",
+  helperText,
+  helperVariant = "alert",
+  ...props
+}: TextFieldProps) => {
+  const helperStyle = useMemo(() => {
+    switch (helperVariant) {
+      case "alert":
+        return "text-red-500";
+      case "success":
+        return "text-emerald-600";
+      default:
+        return "text-black";
+    }
+  }, [helperVariant]);
+
   return (
-    <input
-      {...props}
-      className={`border rounded-sm p-2 border-blueGray-400 text-black ${className}`}
-    />
+    <>
+      <input
+        {...props}
+        className={`border rounded-sm p-2 border-blueGray-400 text-black ${
+          helperText ? "peer" : ""
+        } ${className}`}
+      />
+      {helperText && (
+        <span
+          className={`mt-2 hidden text-sm ${helperStyle} peer-[&:not(:placeholder-shown):not(:focus):invalid]:block`}
+        >
+          {helperText}
+        </span>
+      )}
+    </>
   );
 };
 
--- src/components/TextField/stories.tsx
+++ src/components/TextField/stories.tsx
@@ -13,3 +13,12 @@ export default {
 export const Default: StoryFn<TextFieldProps> = (args) => (
   <TextField {...args} />
 );
+
+export const WithPatternCheck: StoryFn<TextFieldProps> = (args) => (
+  <TextField
+    {...args}
+    pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+    type="email"
+    helperText="Please enter a valid email address"
+  />
+);
