2973a0bd232d33a8ca6ce986707564c7fad0c244
Vini <viniciuszucatti@gmail.com>
2024-10-16 20:51:07 -0300

    Configure min and max connections on knex

--- knexfile.js
+++ knexfile.js
@@ -7,6 +7,7 @@ module.exports = {
     password: "secret_password",
     database: "gamou_db",
   },
+  pool: { min: 2, max: 10 },
   migrations: {
     tableName: "knex_migrations",
     directory: "src/database/migrations",
