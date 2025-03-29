92795adb8b411e18cd98a2e5de86f6e2fb6c82cc
Youmbi Leo (Why-Lord) <Leoyoumbi83@gmail.com>
2025-03-29 22:16:29 +0000

    Remove robots meta tags from authentication pages

--- src/app/(auth)/entrar/page.tsx
+++ src/app/(auth)/entrar/page.tsx
@@ -4,10 +4,6 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Entrar no Gamou",
   description: "Digite seus dados para acessar o sistema.",
-  robots: {
-    index: false,
-    follow: false,
-  },
 };
 
 const Index = async () => {
